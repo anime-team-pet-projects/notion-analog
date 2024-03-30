1f04c16171112e625dfba0490b6f1ba92c71864d
Alex <57665901+miirsery@users.noreply.github.com>
2024-03-30 15:34:00 +0700

    fix(MBR-1): Исправить / Доработать CRUD TODO (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use axum::{routing::{get, post, delete, put}, http::StatusCode, Json, Router};
+use axum::{routing::{get, post, delete, patch}, http::StatusCode, Json, Router};
 use serde::{Deserialize, Serialize};
 use std::{env, sync::Arc};
-use std::convert::Infallible;
+use std::net::IpAddr;
 use sqlx::{Pool, Postgres};
 use sqlx::postgres::{PgPoolOptions};
-use rust_todo::run;
 use axum::extract::{Path, State};
-use axum::response::IntoResponse;
 use chrono::{NaiveDateTime};
 
 #[derive(Clone)]
 struct AppState {
   pool: Pool<Postgres>
+}
+
+pub struct AppConfig {
+  pub app_host: IpAddr,
+  pub app_port: u16,
+  pub app_db_engine: String,
+  pub postgres_db: String,
+  pub postgres_user: String,
+  pub postgres_password: String,
+  pub postgres_host: String,
+}
+
+impl AppConfig {
+  pub fn new() -> Self {
+    let app_host = env::var("APP_HOST").expect("APP_HOST must be set").parse().expect("APP_HOST is not a valid IP address");
+    let app_port = env::var("APP_PORT").expect("APP_PORT must be set").parse().expect("APP_PORT is not a valid port number");
+    let app_db_engine = env::var("APP_DB_ENGINE").expect("APP_DB_ENGINE must be set");
+    let postgres_db = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
+    let postgres_user = env::var("POSTGRES_USER").expect("POSTGRES_USER must be set");
+    let postgres_password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
+    let postgres_host = env::var("POSTGRES_HOST").expect("POSTGRES_HOST must be set");
+
+    AppConfig {
+      app_host,
+      app_port,
+      app_db_engine,
+      postgres_db,
+      postgres_user,
+      postgres_password,
+      postgres_host,
+    }
+  }
+
+  // Функция для создания строки подключения к базе данных
+  pub fn connect_url(&self) -> String {
+    format!(
+      "{}://{}:{}@{}/{}",
+      self.app_db_engine,
+      self.postgres_user,
+      self.postgres_password,
+      self.postgres_host,
+      self.postgres_db
+    )
+  }
 }
 
 #[tokio::main]
@@ -20,26 +62,8 @@ async fn main() {
     .init();
   dotenv::dotenv().ok();
 
-  let app_host = env::var("APP_HOST").expect("APP_HOST must be set");
-  let app_port = env::var("APP_PORT").expect("APP_PORT must be set");
-  let app_db_engine = env::var("APP_DB_ENGINE").expect("APP_DB_ENGINE must be set");
-  let postgres_db = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
-  let postgres_user = env::var("POSTGRES_USER").expect("POSTGRES_USER must be set");
-  let postgres_password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
-  let postgres_host = env::var("POSTGRES_HOST").expect("POSTGRES_HOST must be set");
-
-  let app_db_engine_result: String = app_db_engine.parse().expect("APP_DB_ENGINE is not a valid DB engine");
-  let ip: std::net::IpAddr = app_host.parse().expect("APP_HOST is not a valid IP address");
-  let port: u16 = app_port.parse().expect("APP_PORT is not a valid port number");
-  let postgres_db_result: String = postgres_db.parse().expect("POSTGRES_DB is not a valid database name");
-  let postgres_user_result: String = postgres_user.parse().expect("POSTGRES_USER is not a valid user name");
-  let postgres_password_result: String = postgres_password.parse().expect("POSTGRES_PASSWORD is not a valid password");
-  let postgres_host_result: String = postgres_host.parse().expect("POSTGRES_HOST is not a valid host name");
-
-  let connect_url = format!(
-    "{}://{}:{}@{}/{}",
-    app_db_engine_result, postgres_user_result, postgres_password_result, postgres_host_result, postgres_db_result
-  );
+  let config = AppConfig::new();
+  let connect_url = config.connect_url();
 
   let pool = PgPoolOptions::new()
     .max_connections(5)
@@ -63,9 +87,9 @@ async fn main() {
       let shared_state = shared_state.clone();
       move |id| get_todo(id, shared_state.clone())
     }))
-    .route("/api/v1/todo/:id", put({
+    .route("/api/v1/todo/:id", patch({
       let shared_state = shared_state.clone();
-      move |body| update_todo(body, shared_state.clone())
+      move |id, body| update_todo(id, body, shared_state.clone())
     }))
     .route("/api/v1/todo/:id", delete({
       let shared_state = shared_state.clone();
@@ -73,16 +97,13 @@ async fn main() {
     }))
     .with_state(shared_state);
 
-  let listener = tokio::net::TcpListener::bind(format!("{}:{}", {ip}, {port})).await.unwrap();
-
-  run(ip, port);
+  let listener = tokio::net::TcpListener::bind(format!("{}:{}", {config.app_host}, {config.app_port})).await.unwrap();
 
   let _ = sqlx::migrate!().run(&pool).await;
 
   axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
 async fn root() -> &'static str {
   "Hello, World!"
 }
@@ -94,7 +115,6 @@ async fn create_todo(
   let status = payload.status.unwrap_or("todo".to_string());
   let description = payload.description.unwrap_or("".to_string());
 
-
   let _ = sqlx::query("INSERT INTO todo (title, status, description, user_id) VALUES ($1, $2, $3, $4)")
     .bind(&payload.title)
     .bind(status)
@@ -102,7 +122,7 @@ async fn create_todo(
     .bind(&payload.user_id)
     .execute(&state.pool)
     .await
-    .map_err(|error| (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()))
+    .map_err(internal_error)
     .unwrap();
 
   StatusCode::CREATED
@@ -115,6 +135,7 @@ async fn get_todos(
     .fetch_all(&state.pool)
     .await
     .map_err(internal_error)?;
+
   Ok(Json(todos))
 }
 
@@ -138,34 +159,77 @@ async fn get_todo(
 }
 
 async fn update_todo(
+  Path(id): Path<i32>,
   Json(payload): Json<UpdateTodoDTO>,
   state: Arc<AppState>,
-) -> (StatusCode, String) {
-  let todo =  sqlx::query_as::<_, Todo>("UPDATE todo SET title = $1, status = $2, description = $3 WHERE id = $4 RETURNING id, title")
-    .bind(&payload.title)
-    .bind(&payload.status)
-    .bind(&payload.description)
-    .bind(&payload.id)
-    .fetch_one(&state.pool)
-    .await
-    .map_err(internal_error)
-    .unwrap();
+) -> Result<StatusCode, (StatusCode, String)> {
+  let mut query = String::from("UPDATE todo SET ");
+  let mut bind_values = Vec::new();
+  let mut bind_index = 1;
 
-  (StatusCode::OK, format!("Created todo with id: {}, title: {}", todo.id, todo.title))
+  if let Some(status) = payload.status {
+    query.push_str("status = $");
+    query.push_str(&bind_index.to_string());
+    bind_values.push(status);
+    bind_index += 1;
+  }
+
+  if let Some(title) = payload.title {
+    if !bind_values.is_empty() {
+      query.push_str(", ");
+    }
+    query.push_str("title = $");
+    query.push_str(&bind_index.to_string());
+    bind_values.push(title);
+    bind_index += 1;
+  }
+
+  if let Some(description) = payload.description {
+    if !bind_values.is_empty() {
+      query.push_str(", ");
+    }
+    query.push_str("description = $");
+    query.push_str(&bind_index.to_string());
+    bind_values.push(description);
+    bind_index += 1;
+  }
+
+  if !bind_values.is_empty() {
+    query.push_str(" WHERE id = $");
+    query.push_str(&bind_index.to_string());
+  }
+
+  let mut query_builder = sqlx::query(&query);
+  for value in bind_values {
+    query_builder = query_builder.bind(value);
+  }
+
+  query_builder = query_builder.bind(id);
+
+  let result = query_builder
+    .execute(&state.pool)
+    .await
+    .map_err(internal_error)?;
+
+  if result.rows_affected() > 0 {
+    Ok(StatusCode::OK)
+  } else {
+    Err((StatusCode::NOT_FOUND, "Задача не найдена".to_string()))
+  }
 }
 
 async fn delete_todo(
   Path(id): Path<i32>,
   state: Arc<AppState>,
-) -> Result<impl IntoResponse, Infallible> {
-  let todo = sqlx::query_as::<_, Todo>("DELETE FROM todo WHERE id = $1 RETURNING id, title")
+) -> StatusCode {
+  let _ = sqlx::query("DELETE FROM todo WHERE id = $1")
     .bind(id)
-    .fetch_one(&state.pool)
+    .execute(&state.pool)
     .await
-    .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Error fetching todo".to_string()))
+    .map_err(internal_error)
     .unwrap();
 
-  Ok(Json(todo))
+  StatusCode::OK
 }
 
 fn internal_error<E>(err: E) -> (StatusCode, String)
@@ -185,10 +249,9 @@ struct CreateTodoDTO {
 
 #[derive(Deserialize, Debug)]
 struct UpdateTodoDTO {
-  id: i32,
-  title: String,
-  status: String,
-  description: String,
+  title: Option<String>,
+  status: Option<String>,
+  description: Option<String>,
 }
 
 #[derive(Serialize, Debug, sqlx::FromRow)]
